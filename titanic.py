2e907ded46dae0bde29d6f6192c8785119ecebfd
rtalyansky <leah.talyansky@gmail.com>
2015-02-17 17:42:19 -0800

    clear weird whitespaces

--- titanic.py
+++ titanic.py
@@ -12,18 +12,19 @@ sql = "INSERT INTO passengers VALUES (?, ?, ?, ?)" #sqlite formater that you can
 
 lineno = 0
 with open('ch03_titanic.csv') as csvfile:
-    for row in csvfile: #reads line by line 
-        lineno += 1
-        r = row.strip().split(',') #strip eliminates line ends/spaces, split makes a list  
-        t = tuple(r) 
-        if lineno == 1: #only the first line in special 
-            sqlcreate = sqlcreate_format % t #tuple 
-            #print(sqlcreate)
-            c.execute(sqlcreate)
-        else:
-            #print(t)
-            #print(sql)
-            c.execute(sql, t) 
+    for row in csvfile: #reads line by line
+        lineno += 1
+        r = row.strip().split(',') #strip eliminates line ends/spaces, split makes a list  
+        t = tuple(r) 
+        if lineno == 1: #only the first line in special 
+            sqlcreate = sqlcreate_format % t #tuple 
+            #print(sqlcreate)
+            c.execute(sqlcreate)
+        else:
+            #print(t)
+            #print(sql)
+            c.execute(sql, t) 
 
 conn.commit()
 conn.close()
+
