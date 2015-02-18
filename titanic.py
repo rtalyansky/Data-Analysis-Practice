a835728afc706a16623d3a61c100a97d3dd241e8
rtalyansky <leah.talyansky@gmail.com>
2015-02-17 17:47:49 -0800

    improved comments in code

--- titanic.py
+++ titanic.py
@@ -3,27 +3,27 @@ import sqlite3
 conn = sqlite3.connect('titanic.db')
 c = conn.cursor() #context in which you execute sql commands 
 
-c.execute('drop table if exists passengers')
+c.execute('drop table if exists passengers') # delete table from db, if exists, otherwise create table will fail
 
 sqlcreate_format = '''CREATE TABLE passengers (%s CHAR default "", %s integer default 0, %s CHAR default "", %s CHAR default "")'''
 
-sql = "INSERT INTO passengers VALUES (?, ?, ?, ?)" #sqlite formater that you can insert tuple of values 
-#executemany lets you insert multiple tupls 
+sql = "INSERT INTO passengers VALUES (?, ?, ?, ?)" #sqlite formatter will insert values from tuple
 
 lineno = 0
 with open('ch03_titanic.csv') as csvfile:
     for row in csvfile: #reads line by line
         lineno += 1
-        r = row.strip().split(',') #strip eliminates line ends/spaces, split makes a list  
-        t = tuple(r) 
-        if lineno == 1: #only the first line in special 
+        r = row.strip().split(',') #strip eliminates line ends/spaces, split separates input line by commas, makes a list
+        t = tuple(r) # produce tuple out of a list, to pass it to format statement
+        if lineno == 1: #first line is special: contains names of columns
             sqlcreate = sqlcreate_format % t #tuple 
             #print(sqlcreate)
             c.execute(sqlcreate)
-        else:
+        else: # rest of the lines are data rows
             #print(t)
             #print(sql)
             c.execute(sql, t) 
+            #executemany lets you insert multiple tuples 
 
 conn.commit()
 conn.close()
