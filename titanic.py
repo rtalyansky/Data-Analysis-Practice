3973f3b15904b91407b1b5273872d355f3a9ef61
kelleydv <kelleydv@gmail.com>
2015-02-23 17:04:47 -0800

    abstract into module

--- titanic.py
+++ titanic.py
@@ -1,30 +1,43 @@
 import sqlite3
 
-conn = sqlite3.connect('titanic.db')
-c = conn.cursor() #context in which you execute sql commands 
 
-c.execute('drop table if exists passengers') # delete table from db, if exists, otherwise create table will fail
+def write_table(db=None, table=None, filepath=None):
+    """Populate a table in db given a csv file.  All parameters are strings."""
+    
+    if not db.endswith('.db'):
+        db += '.db'
 
-sqlcreate_format = '''CREATE TABLE passengers (%s CHAR default "", %s integer default 0, %s CHAR default "", %s CHAR default "")'''
+    conn = sqlite3.connect(db)
+    c = conn.cursor() #context in which you execute sql commands 
 
-sql = "INSERT INTO passengers VALUES (?, ?, ?, ?)" #sqlite formatter will insert values from tuple
+    c.execute('drop table if exists %s' % table) # delete table from db, if exists, otherwise create table will fail
 
-lineno = 0
-with open('ch03_titanic.csv') as csvfile:
-    for row in csvfile: #reads line by line
-        lineno += 1
-        r = row.strip().split(',') #strip eliminates line ends/spaces, split separates input line by commas, makes a list
-        t = tuple(r) # produce tuple out of a list, to pass it to format statement
-        if lineno == 1: #first line is special: contains names of columns
-            sqlcreate = sqlcreate_format % t #tuple 
-            #print(sqlcreate)
-            c.execute(sqlcreate)
-        else: # rest of the lines are data rows
-            #print(t)
-            #print(sql)
-            c.execute(sql, t) 
-            #executemany lets you insert multiple tuples 
+    sqlcreate_format = 'CREATE TABLE ' + table + ''' (%s CHAR default "", %s integer default 0, %s CHAR default "", %s CHAR default "")'''
 
-conn.commit()
-conn.close()
+    sql = "INSERT INTO %s VALUES (?, ?, ?, ?)" % table #sqlite formatter will insert values from tuple
+
+    lineno = 0
+    with open(filepath) as csvfile:
+        for row in csvfile: #reads line by line
+            lineno += 1
+            r = row.strip().split(',') #strip eliminates line ends/spaces, split separates input line by commas, makes a list
+            t = tuple(r) # produce tuple out of a list, to pass it to format statement
+            if lineno == 1: #first line is special: contains names of columns
+                sqlcreate = sqlcreate_format % t #tuple 
+                #print(sqlcreate)
+                c.execute(sqlcreate)
+            else: # rest of the lines are data rows
+                #print(t)
+                #print(sql)
+                c.execute(sql, t) 
+                #executemany lets you insert multiple tuples 
+
+    conn.commit()
+    conn.close()
+
+
+def titanic_data():
+    """Specific case for titanic data"""
+    write_table(db='titanic.db', table='passengers', filepath='ch03_titanic.csv')
+
 
